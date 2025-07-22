f5f3ab9c21ebb7aeb61713be7edecc1b3e2ee3ca
Raygoza <rraygoza1408@gmail.com>
2025-07-21 20:16:14 -0600

    Change on postbuild script

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
+ 
 rm -rf ./.amplify-hosting
-
-mkdir -p ./.amplify-hosting/compute/default
-
-# For JavaScript, copy the source files directly (no dist folder needed)
-cp *.js ./.amplify-hosting/compute/default/
-cp package.json ./.amplify-hosting/compute/default/
+ 
+mkdir -p ./.amplify-hosting/compute
+ 
+cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
-cp .env ./.amplify-hosting/compute/default/ 2>/dev/null || true
-
-# Create public directory if it doesn't exist
-mkdir -p public
+ 
 cp -r public ./.amplify-hosting/static
-
+ 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
